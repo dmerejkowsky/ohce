97129c0bb0dffbd3f1dd85c08dd60c4ebe3bc69a
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-11-26 19:12:59 +0100

    A tiny bit of defensive coding

--- app/src/main/java/info/dmerej/ohce/HourMapper.java
+++ app/src/main/java/info/dmerej/ohce/HourMapper.java
@@ -1,17 +1,24 @@
 package info.dmerej.ohce;
 
+import java.security.InvalidParameterException;
+
 public class HourMapper {
   public static Time map(int hours) {
+    if (hours < 0) {
+      throw new InvalidParameterException("hour cannot be negative");
+    }
+    if (hours > 24) {
+      throw new InvalidParameterException("hour cannot be greater than 24");
+    }
+
     if (hours >= 6 && hours <= 12) {
       return Time.MORNING;
     }
-    if (hours >= 20 || hours <= 6) {
-      return Time.NIGHT;
-    }
+
     if (hours > 12 && hours < 20) {
       return Time.AFTERNOON;
     }
 
-    throw new RuntimeException("Unreachable code");
+    return Time.NIGHT;
   }
 }
--- app/src/test/java/info/dmerej/ohce/HourMapperTest.java
+++ app/src/test/java/info/dmerej/ohce/HourMapperTest.java
@@ -2,7 +2,10 @@ package info.dmerej.ohce;
 
 import org.junit.jupiter.api.Test;
 
+import java.security.InvalidParameterException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class HourMapperTest {
   @Test
@@ -17,11 +20,25 @@ public class HourMapperTest {
 
   @Test
   void test5HoursIsNightTime() {
-    assertEquals(Time.NIGHT, HourMapper.map(21));
+    assertEquals(Time.NIGHT, HourMapper.map(5));
   }
 
   @Test
   void test6hoursIsMorning() {
     assertEquals(Time.MORNING, HourMapper.map(6));
+  }
+
+  @Test
+  void rejectNegativeHours() {
+    assertThrows(InvalidParameterException.class, () -> {
+      HourMapper.map(-1);
+    });
+  }
+
+  @Test
+  void reject25tHour() {
+    assertThrows(InvalidParameterException.class, () -> {
+      HourMapper.map(25);
+    });
   }
 }
