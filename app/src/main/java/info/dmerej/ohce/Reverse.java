9f1dacb28cc7808d09a65a704d01574e7d4cc27c
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-11-30 10:44:43 +0100

    StringBuilder has a reverse() method, use it

--- app/src/main/java/info/dmerej/ohce/Reverse.java
+++ app/src/main/java/info/dmerej/ohce/Reverse.java
@@ -3,12 +3,8 @@ package info.dmerej.ohce;
 public class Reverse {
 
   public static String reverse(String input) {
-    StringBuilder builder = new StringBuilder();
-    for (int i = input.length() - 1; i >= 0; i--) {
-      int charValue = input.charAt(i);
-      char c = (char) charValue;
-      builder.append(c);
-    }
+    StringBuilder builder = new StringBuilder(input);
+    builder.reverse();
     return builder.toString();
   }
 }
